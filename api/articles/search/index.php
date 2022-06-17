f80a6a852ec6fc95cb164324e7d864b69ec0b30d
Xhevat <xhevat.ziberi@gmail.com>
2022-06-17 18:02:17 +0200

    new products

--- api/articles/search/index.php
+++ api/articles/search/index.php
@@ -1,5 +1,30 @@
 <?php
-$data = file_get_contents('data.json');
 header('Content-Type: application/json; charset=utf-8');
-echo $data;
+$json = file_get_contents('data.json');
+// echo $json; return;
+$json = json_decode($json);
+$q = json_decode( $_GET['q'] );
+
+$query = '';
+if ( isset( $q->{'$or'} ) ) {
+  if ( isset( $q->{'$or'}[0] ) ) {
+    if ( isset( $q->{'$or'}[0]->{'$like'} ) ) {
+      if ( isset( $q->{'$or'}[0]->{'$like'}[1] ) ) {
+        $query = isset( $q->{'$or'}[0]->{'$like'}[1];
+      }
+    }
+  }
+}
+
+$arr = [];
+foreach ($json->data as $key => $data) {
+  if ( $data->uuid == $brand ) {
+    array_push($arr, $json->data[$key]);
+  }
+}
+
+$json->data = $arr;
+$json->meta->total = count($json->data);
+
+echo json_encode( $json );
 return;
